47d67ca7a08061d82e374c8cf3539f048b87b097
bhiswes <86588842+bhiswes@users.noreply.github.com>
2021-09-08 12:23:07 +0530

    Create unmineable.sh

---
+++ unmineable.sh
@@ -0,0 +1,6 @@
+# unmineable
+sudo apt update
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
+tar -xf lolMiner_v1.29_Lin64.tar.gz
+cd 1.29
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user BTC:33mCCoZBspfeER42ZBe5tJfjrYufimf7pG.worker6 --ethstratum ETHPROXY
